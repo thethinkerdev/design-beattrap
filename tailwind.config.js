6adb0386411f2a1e3684f59c932d249c284f295e
samankhdev <samkh.dev@yahoo.com>
2022-02-16 04:53:12 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode:"class",
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      colors:{
+      "darkBlack":"#12111f"
+      }
+    },
+  },
+  plugins: [],
+}
