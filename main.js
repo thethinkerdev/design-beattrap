04ec6b44557ac9325cf328d27a46751fafcbed48
Saman <mafhh.end@gmail.com>
2022-02-21 18:02:25 +0330

    iniit

---
+++ main.js
@@ -0,0 +1,47 @@
+
+  let ElShadow = document.querySelector(".shadow");
+  let EltoggleBar = document.querySelector("#toggle-bar");
+  let ElMainNavAccountProfile = document.querySelector(
+    "#main-nav-profile"
+  );
+
+  const ActiveShadow=()=>{
+    ElShadow.classList.add("active")
+  }
+  const DeActiveShadow=()=>{
+   ElShadow.classList.remove("active")
+  }
+
+
+  document.querySelector("#sidebar-close").addEventListener("click", () => {
+    document.querySelector("#sidebar").classList.toggle("active");
+    DeActiveShadow();
+  });
+
+  EltoggleBar.addEventListener("click", () => {
+    document.querySelector("#sidebar").classList.toggle("active");
+    ActiveShadow();
+  });
+
+  document.addEventListener("click", (e) => {
+    if (e.target.closest("#main-nav-profile")) {
+      ElMainNavAccountProfile.querySelector("ul").classList.toggle("active");
+    ActiveShadow();
+
+    } else {
+      ElMainNavAccountProfile.querySelector("ul").classList.remove("active");
+    }
+    
+    if (e.target.closest(".shadow")) {
+      document.querySelector("#sidebar").classList.remove("active");
+      DeActiveShadow();
+    }
+  });
+
+
+  const toggleDayLight = () => {
+    if (localStorage.getItem("dark-mode") == "true") {
+      localStorage.setItem("dark-mode", false);
+      document.querySelector("html").classList.remove("dark");
+    } else localStorage.setItem("dark-mode", true);
+  };
