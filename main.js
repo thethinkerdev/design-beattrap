e1b9592f079558756b6b548497dd6bd47c6d30fc
mafhhend <mafhh.end@gmail.com>
2022-02-24 23:18:40 +0330

    add menu

--- main.js
+++ main.js
@@ -1,47 +1,34 @@
+let ElShadow = document.querySelector(".shadow");
+let EltoggleBar = document.querySelector("#toggle-bar");
+let ElMainNavAccountProfile = document.querySelector("#main-nav-profile");
 
-  let ElShadow = document.querySelector(".shadow");
-  let EltoggleBar = document.querySelector("#toggle-bar");
-  let ElMainNavAccountProfile = document.querySelector(
-    "#main-nav-profile"
-  );
+const ActiveShadow = () => {
+  ElShadow.classList.add("active");
+};
+const DeActiveShadow = () => {
+  ElShadow.classList.remove("active");
+};
 
-  const ActiveShadow=()=>{
-    ElShadow.classList.add("active")
+document.querySelector("#sidebar-close").addEventListener("click", () => {
+  document.querySelector("#sidebar").classList.toggle("active");
+  DeActiveShadow();
+});
+
+EltoggleBar.addEventListener("click", () => {
+  document.querySelector("#sidebar").classList.toggle("active");
+  ActiveShadow();
+});
+
+document.addEventListener("click", e => {
+  if (e.target.closest("#main-nav-profile")) {
+    ElMainNavAccountProfile.querySelector("ul").classList.toggle("active");
+    ActiveShadow();
+  } else {
+    ElMainNavAccountProfile.querySelector("ul").classList.remove("active");
   }
-  const DeActiveShadow=()=>{
-   ElShadow.classList.remove("active")
-  }
 
-
-  document.querySelector("#sidebar-close").addEventListener("click", () => {
-    document.querySelector("#sidebar").classList.toggle("active");
+  if (e.target.closest(".shadow")) {
+    document.querySelector("#sidebar").classList.remove("active");
     DeActiveShadow();
-  });
-
-  EltoggleBar.addEventListener("click", () => {
-    document.querySelector("#sidebar").classList.toggle("active");
-    ActiveShadow();
-  });
-
-  document.addEventListener("click", (e) => {
-    if (e.target.closest("#main-nav-profile")) {
-      ElMainNavAccountProfile.querySelector("ul").classList.toggle("active");
-    ActiveShadow();
-
-    } else {
-      ElMainNavAccountProfile.querySelector("ul").classList.remove("active");
-    }
-    
-    if (e.target.closest(".shadow")) {
-      document.querySelector("#sidebar").classList.remove("active");
-      DeActiveShadow();
-    }
-  });
-
-
-  const toggleDayLight = () => {
-    if (localStorage.getItem("dark-mode") == "true") {
-      localStorage.setItem("dark-mode", false);
-      document.querySelector("html").classList.remove("dark");
-    } else localStorage.setItem("dark-mode", true);
-  };
+  }
+});
