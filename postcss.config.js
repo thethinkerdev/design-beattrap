fbdb521f441dfc01bf63ce93c6f48f273614c317
thethinkerdev <mafhh.end@gmail.com>
2022-11-15 18:07:42 +0330

    using postcss instead of sass and Organize the project directories

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   plugins:[
+    require("postcss-import"),
     require("tailwindcss"),
     require("autoprefixer"),
     require("cssnano"),
     require("postcss-nested"),
   ],
+  "browserslist": [
+    "last 3 versions"
+ ]
 }
