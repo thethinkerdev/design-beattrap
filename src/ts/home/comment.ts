c0145488af1a504aefb48e90bcbe04114a2fb2d6
thethinkerdev <mafhh.end@gmail.com>
2022-11-30 12:45:28 +0330

    fix comment reply answer toggle

--- src/ts/home/comment.ts
+++ src/ts/home/comment.ts
@@ -1,10 +1,13 @@
 document
-.querySelectorAll(".product__comments__comment__reply")
-.forEach((reply) => {
-  reply.addEventListener("click", (e) => {
-    const replySpanElement = e.currentTarget as HTMLSpanElement;
-    replySpanElement.parentElement
-      ?.querySelector(".product__comments__comment__reply-this-comment")
-      ?.classList.toggle("hidden");
+  .querySelectorAll(".product__comments__comment__reply")
+  .forEach((reply) => {
+    reply.addEventListener("click", (e) => {
+      const replySpanElement = e.currentTarget as HTMLSpanElement;
+
+      console.log(replySpanElement);
+      
+      replySpanElement.parentElement
+        ?.querySelector(".product__comments__comment__reply-this-comment")
+        ?.classList.toggle("product__comments__comment__reply-this-comment--active");
+    });
   });
-});
