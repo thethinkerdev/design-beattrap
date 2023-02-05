902e653aea658a3544f2cc0fd62665c49fb60451
thethinkerdev <mafhh.end@gmail.com>
2023-02-05 16:28:45 +0330

    complete nav, working on sidebar

--- src/home/ts/index.ts
+++ src/home/ts/index.ts
@@ -1,2 +1,17 @@
-//global
+//CSS: global
 import "../../sass/index.scss"
+//CSS: home
+import "./../../home/sass/index.scss"
+
+
+
+const angleArrow = document.querySelector('.nav__arrow-right') as HTMLDivElement
+const sidebar = document.querySelector(".sidebar") as HTMLDivElement
+const container = document.querySelector(".container") as HTMLDivElement
+
+angleArrow.addEventListener('click',(e:any)=>{
+    e.target.className ="fa fa-angle-left";
+    sidebar.classList.toggle('sidebar--active')
+
+    container.classList.toggle("container--sidebar-active")
+})
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,22 @@ module.exports = {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'dark':{
+          50: '#edecec',
+          100: '#d1d1d1',
+          200: '#b2b2b2',
+          300: '#9c9c9c',
+          400: '#7b7b7b',
+          500: '#5c5c5c',
+          600: '#3b3b3b',
+          700: '#313131',
+          800: '#1f1f1f',
+          900: '#0d0d0d',
+        }
+      }
+    },
   },
   plugins: [],
 }
